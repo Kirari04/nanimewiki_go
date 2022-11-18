805319b8b4c100d4ffbf32983bb5fed429e7f175
Kirari04 <lev@saminskij.ch>
2022-11-18 21:36:50 +0100

    added email controller example

--- controllers/userRegisterController.go
+++ controllers/userRegisterController.go
@@ -2,17 +2,8 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/mailgun/mailgun-go/v4"
 )
 
-func SendSimpleMessage(c *gin.Context, domain, apiKey string) (string, error) {
-	mg := mailgun.NewMailgun(domain, apiKey)
-	m := mg.NewMessage(
-		"Excited User <mailgun@YOUR_DOMAIN_NAME>",
-		"Hello",
-		"Testing some Mailgun awesomeness!",
-		"YOU@YOUR_DOMAIN_NAME",
-	)
-	_, id, err := mg.Send(c, m)
-	return id, err
+func UserRegister(c *gin.Context) {
+
 }
