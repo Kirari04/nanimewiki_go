961368f27c9cab18d48396179a36d5e91604a728
Kirari04 <lev@saminskij.ch>
2022-11-19 17:42:36 +0100

    made email controller functional

--- controllers/emailController.go
+++ controllers/emailController.go
@@ -7,14 +7,14 @@ import (
 	"github.com/mailgun/mailgun-go/v4"
 )
 
-func SendSimpleMessage(c *gin.Context) (string, error) {
+func SendMessage(c *gin.Context, header string, message string, receiver string) (string, error) {
 	mg := mailgun.NewMailgun(os.Getenv("mailgun_domain"), os.Getenv("mailgun_apikey"))
 	mg.SetAPIBase(os.Getenv("mailgun_apibase"))
 	m := mg.NewMessage(
-		"John <"+os.Getenv("mailgun_sender")+">",
-		"Hello",
-		"Testing some Mailgun awesomeness!",
-		"receiver@example.com",
+		os.Getenv("mailgun_sender_name")+" <"+os.Getenv("mailgun_sender")+">",
+		header,
+		message,
+		receiver,
 	)
 	_, id, err := mg.Send(c, m)
 	return id, err
