070e2b70cc2eea250537ceab0973146b1818a4c8
Kirari04 <lev@saminskij.ch>
2022-11-19 12:17:08 +0100

    improoved performance & cleened up code

---
+++ controllers/animeListController.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"ch/kirari/animeApi/models"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type MapRequest struct {
+	Index uint `uri:"index"`
+}
+
+func ListAnime(c *gin.Context) {
+	// VALIDATOR
+	var req MapRequest
+	if c.ShouldBindUri(&req) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"success": 0,
+			"error":   "Bad Request",
+			"data":    nil,
+			"len":     nil,
+		})
+		return
+	}
+
+	// LOGIC
+	limit, _ := strconv.Atoi(os.Getenv("api_lenlist"))
+	offset := int(req.Index) * limit
+
+	var animes []models.Anime
+	models.DB.Limit(limit).Offset(offset).Find(&animes)
+	var itemLen int64
+	models.DB.Model(&models.Anime{}).Count(&itemLen)
+
+	c.JSON(http.StatusOK, gin.H{
+		"success": 1,
+		"error":   "",
+		"data":    animes,
+		"len":     itemLen,
+	})
+}
