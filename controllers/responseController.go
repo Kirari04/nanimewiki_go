1937adc201ad1e29f7ff88c9d2ee7bf2d1a4f794
Kirari04 <lev@saminskij.ch>
2022-11-19 18:57:18 +0100

    added prewritten responses for more cleen code

---
+++ controllers/responseController.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InternalServerError_response(c *gin.Context, err error) {
+	c.JSON(http.StatusInternalServerError, gin.H{
+		"success": 0,
+		"error":   "Database Error",
+		"data":    nil,
+		"len":     nil,
+	})
+}
