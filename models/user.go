1f19ced1e1c4b40dfcb40bcdc48db479e3e245ec
Kirari04 <lev@saminskij.ch>
2022-11-20 13:11:24 +0100

    added verified check

--- models/user.go
+++ models/user.go
@@ -1,11 +1,12 @@
 package models
 
 type User struct {
-	ID       uint   `gorm:"primary_key;autoIncrement;not null;unique"`
-	Email    string `gorm:"not null;uniqueIndex"`
-	Username string `gorm:"not null;index"`
-	Password string `gorm:""`
-	Avatar   string `gorm:""`
-	Updated  int64  `gorm:"autoUpdateTime"` // Use unix milli seconds as updating time
-	Created  int64  `gorm:"autoCreateTime"`
+	ID            uint   `gorm:"primary_key;autoIncrement;not null;unique"`
+	Email         string `gorm:"not null;uniqueIndex"`
+	EmailVerified bool   `gorm:"default:false"`
+	Username      string `gorm:"not null;index"`
+	Password      string `gorm:""`
+	Avatar        string `gorm:""`
+	Updated       int64  `gorm:"autoUpdateTime"` // Use unix milli seconds as updating time
+	Created       int64  `gorm:"autoCreateTime"`
 }
